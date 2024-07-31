27e76bbd084a6f5d4e241e8712587f29b82c2425
Sebastian Kefer <sebastian.kefer@gmail.com>
2024-07-31 17:42:09 +0000

    added httpresponse for testing purposes

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is going to be the blog page.")
+    return HttpResponse("This is going to be the blog page. One day it will!")
